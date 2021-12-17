74b2de371ae3a3f75178910346d16f92cde17e15
surfruit <surfruitsup@gmail.com>
2021-12-17 11:24:25 +0200

    starting the project

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+import PersonIcon from '@mui/icons-material/Person';
+
+function Header() {
+    return (
+        <div className='header'>
+           <h2>I am header</h2> 
+           <PersonIcon />
+        </div>
+    )
+}
+
+export default Header
